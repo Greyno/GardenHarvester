6e40282136f5383b20eb56ce453bcfc4b581fc1f
greyno <gamrey@mat-mac.com>
2017-02-23 20:10:52 -0500

    Added buttons to main page

---
+++ src/pages/existing/existing.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+/*
+  Generated class for the Existing page.
+
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
+@Component({
+  selector: 'page-existing',
+  templateUrl: 'existing.html'
+})
+export class ExistingPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ExistingPage');
+  }
+
+}
